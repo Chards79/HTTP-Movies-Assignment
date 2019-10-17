d2c93d96866978bddf2ed94dfa6c981cd895df56
Eric Richards <esholundr@yahoo.com>
2019-10-17 16:43:16 -0600

    Added dependencies

---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+const initialMovie = {
+    title: '',
+    director: '',
+    metascore: '',
+    stars: ['']
+};
+
+export default UpdateMovieForm;
